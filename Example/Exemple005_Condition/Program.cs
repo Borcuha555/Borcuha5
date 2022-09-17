b3812e22ad5cae2a0f1d6c694231626b554de8d7
Vladislav <borcuhawrestler555@gmail.com>
2022-09-17 22:26:49 +0300

    commit6

---
+++ Example/Exemple005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "василиса")
+{
+    Console.WriteLine("Я Тебя Люблю!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
